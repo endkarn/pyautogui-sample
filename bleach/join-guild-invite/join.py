ad6c48e47f5a2b0a9f3cb468dba0523f08769bcb
endkarn <end_karn@hotmail.com>
2022-08-08 15:36:02 +0700

    Add state inLobby for prevent repeat guild chat

--- bleach/join-guild-invite/join.py
+++ bleach/join-guild-invite/join.py
@@ -5,7 +5,7 @@ import keyboard
 
 try:
     cur_mouse = pyautogui.position()
-
+    inLobby = False
 
     def findAndClick(image, cond, lable):
         imageLocation = pyautogui.locateOnScreen(image, confidence=cond, grayscale=True)
@@ -29,20 +29,24 @@ try:
             break
 
 
+    
+
         # Resolution Windows 1024x576 /w Lobby Screen
-        if findAndClick('title.png', 0.8, 'Room') is False:
+        if inLobby is False:
             findAndClick('noti.png', 0.8, 'Click Guild Noti')
         else:
             findAndClick('5ticket.png', 0.9, 'Active x5')
             findAndClick('ready5x.png', 0.5, 'Ready')
         
-        findAndClick('join.png', 0.8, 'Join Room')
+        if findAndClick('join.png', 0.8, 'Join Room') is True:
+            inLobby = True
+
         findAndClick('tap-screen.png', 0.8, 'TAP SCREEN')
         findAndClick('items-obtained.png', 0.8, 'Items Obtained')
         findAndClick('result-screen.png', 0.8, 'Result Screen')
-        findAndClick('coop.png', 0.9, 'Co-Op Quest Menu')
 
-
+        if findAndClick('coop.png', 0.9, 'Co-Op Quest Menu') is True:
+            inLobby = False
 
         time.sleep(0.05)
 except KeyboardInterrupt:
